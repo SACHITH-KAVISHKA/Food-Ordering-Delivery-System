5c815ec254008d58c9b7eba78c4357f00ca4c042
it22578532 <heshanjeewantha@gmail.com>
2025-04-25 16:46:23 +0530

    Get assigned or available orders routes

---
+++ deliveri-services/routes/delivery.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router();
+const axios = require('axios');
+const { verifyToken, allowRoles } = require('../utils/authMiddleware');
+
+const ORDER_SERVICE_URL = process.env.ORDER_SERVICE_URL;
+
+// 🚚 Get assigned or available orders
+router.get('/orders', verifyToken, allowRoles('delivery'), async (req, res) => {
+  try {
+    const response = await axios.get(`http://order-service:5003/order/delivery/orders`, {
+      headers: {
+        Authorization: req.headers.authorization
+      }
+    });
+    res.json(response.data);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).json({ message: 'Error fetching delivery orders' });
+  }
+});
+
+
+
+module.exports = router;
